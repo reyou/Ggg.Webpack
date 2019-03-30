54fb414fec1859c9fc17642b939474e44c8f2d1f
Teoman Shipahi <aytekoz@yahoo.com>
2019-03-30 10:06:18 -0400

    files moved

--- apps/app-youtube-LearnCode.academy/js/module1.js
+++
@@ -1,3 +0,0 @@
-var $  = require("jquery");
-console.log("this is module 1.");
-$("#module1").html("this is message from module1.js.");
--- apps/app-youtube-LearnCode.academy/js/scripts.js
+++
@@ -1,3 +0,0 @@
-// this will not work in the browser
-require('./module1.js');
-require('./module2.js');
